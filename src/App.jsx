289e72f46d16ddc790e92a6da160fa453f63de98
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-04-26 13:24:41 +0600

    simple firebase practice login by google

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from './firebase/firebase.config';
+
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+function App() {
+  const [user, setUser] = useState(null);
+  
+
+  const handleGoogleSignIn = () =>{
+    signInWithPopup(auth, googleProvider)
+    .then(result =>{
+      const loogedUser = result.user
+      console.log(loogedUser);
+      setUser(loogedUser)
+
+    })
+    .catch(error =>{
+      console.log(error);
+    })
+
+  }
+  return (
+    <div className="App">
+      <h1>Firebase + React</h1>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {user &&
+      <div className='card'>
+        <h4>User: {user.displayName}</h4>
+      </div>
+
+      }
+    
+    </div>
+  )
+}
+
+export default App
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIq15AhrNGBGSyHXcSSdOeQRPdnuCtg5I",
+  authDomain: "dimple-firebase-app-f8995.firebaseapp.com",
+  projectId: "dimple-firebase-app-f8995",
+  storageBucket: "dimple-firebase-app-f8995.appspot.com",
+  messagingSenderId: "914190624701",
+  appId: "1:914190624701:web:737fdbaf6bbd5980ef14d4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
